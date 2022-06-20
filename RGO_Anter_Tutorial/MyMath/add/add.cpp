adb1f3e2d760ffdee22c24a3e78275a33f92d69b
danoneyal <eyal.danon@rgorobotics.ai>
2022-06-20 09:09:09 +0300

    add cpp and txt to git

---
+++ RGO_Anter_Tutorial/MyMath/add/add.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// a hack square root calculation using simple operations
+double add(double x,double y)
+{  
+  double result = x+y;
+  std::cout << "Computing the add of " << x << "and "<< y <<" to be " << result << std::endl;
+  return result;
+}
